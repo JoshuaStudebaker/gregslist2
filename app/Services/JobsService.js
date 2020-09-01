460886d1877ca9a1706fe3077ad85ded534723f3
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-01 13:59:11 -0600

    skeleton jobsservice added

---
+++ app/Services/JobsService.js
@@ -0,0 +1,8 @@
+import { ProxyState } from "../AppState.js";
+import Job from "../Models/Job.js";
+import { api } from "../Services/AxiosService.js";
+
+class JobsService {}
+
+const SERVICE = new JobsService();
+export default SERVICE;
