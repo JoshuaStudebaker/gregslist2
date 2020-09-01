27478c71aa8ee5f06d22be0e139237b3dad2e0e3
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-01 14:01:44 -0600

    skeleton jobscontroller added

---
+++ app/Controllers/JobsController.js
@@ -0,0 +1,8 @@
+import { ProxyState } from "../AppState.js";
+import jobsService from "../Services/JobsService.js";
+
+function _drawJobs() {}
+
+export default class JobsController {
+  constructor() {}
+}
