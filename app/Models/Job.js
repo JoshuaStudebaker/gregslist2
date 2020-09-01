0165a48e9d415d6a94d18685dcf435fb1eacf28f
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-01 16:57:54 -0600

    formated html load

--- app/Models/Job.js
+++ app/Models/Job.js
@@ -12,13 +12,16 @@ export default class Job {
     return `
     
     <div class="card m-2 shadow border-dark" style="width: 18rem;">
-  <div class="card-body">
+  <div class="card-header" style="height: 6.75rem;">
     <h5 class="card-title">${this.jobTitle} - ${this.company}</h5>
     <h6 class="card-subtitle mb-2 text-muted">$${this.rate}/hr - ${this.hours}hrs/week</h6>
+    </div>
+  <div class="card-body" style="height-min: 6.75rem;">
     <p class="card-text">${this.description}</p>
+    </div>
+    <div class="card-footer">
     <button type="button" class="btn btn-secondary" onclick="app.jobsController.removeJobPosting('${this.id}')")>Remove Job</button>
-    
-  </div>
+    </div>  
 </div>
     `;
   }
